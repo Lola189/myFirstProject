907e08b109ab054cd91a6bce5f8ec4e7858820a3
Lolita Bartosh <lola7565762@gmail.com>
2024-11-20 17:52:49 +0300

    выполнен урок 01

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const title = 10;
-const screens = 11;
-const screenPrice = 12;
-const rollback = 13;
-const fullPrice = 14;
-const adaptive = 15;
+const title = 10
+const screens = 11
+const screenPrice = 12
+const rollback = 13
+const fullPrice = 14
+const adaptive = 15
+
 
 console.log("Hello console");
 alert("Hello World")
