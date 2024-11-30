ae00f9dcd5672997bf3f7aa80a11ef4bc99461f7
Lolita Bartosh <lola7565762@gmail.com>
2024-11-30 18:23:24 +0300

    выполнен урок 03

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-const title = "My First Project";
-const screens = "Простые, Сложные, Интерактивные";
-const screenPrice = 30;
+'use strict'
+
+const title = prompt('Как называется ваш проект?');
+const screens = prompt('Какие типы экранов нужно разработать?', "Простые, Сложные, Интерактивные");
+const screenPrice = +prompt('Сколько будет стоить данная работа?', "120");
 const rollback = 10;
-const fullPrice = 50;
-const adaptive = true;
+const adaptive = confirm('Нужен ли адаптив на сайте?');
+
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrise1 = +prompt('Сколько это будет стоить?');
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrise2 = +prompt('Сколько это будет стоить?');
+
+const fullPrice = screenPrice + servicePrise1 + servicePrise2;
+const rollInter = fullPrice*(rollback/100);
+const servicePercentPrice = Math.ceil(fullPrice - rollInter);
+console.log(servicePercentPrice);
+
+if (fullPrice >= 300) {
+    console.log("Даем скидку в 10%");
+} else if (150 <= fullPrice && fullPrice < 300) {
+    console.log("Даем скидку в 5%")
+} else if (0 < fullPrice && fullPrice < 150) {
+    console.log("Скидка не предусмотрена")
+} else {
+    console.log("Что то пошло не так")
+}
 
 console.log(typeof title);
 console.log(typeof fullPrice);
